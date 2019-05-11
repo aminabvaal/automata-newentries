2d277b345ae0b5882dd3bb21b519b11173c65719
aminabvaal <ag1992@aut.ac.ir>
2019-05-11 17:23:25 +0430

    Initial commit

---
+++ src/main/java/ir/ac/aut/god/automatanewentries/AutomataNewentriesApplication.java
@@ -0,0 +1,13 @@
+package ir.ac.aut.god.automatanewentries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomataNewentriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomataNewentriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/ac/aut/god/automatanewentries/AutomataNewentriesApplicationTests.java
@@ -0,0 +1,16 @@
+package ir.ac.aut.god.automatanewentries;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutomataNewentriesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
