4c93cf6c532b294031418c0a018725a729f7596d
aminabvaal <ag1992@aut.ac.ir>
2019-07-12 16:10:56 +0430

    basic classes're adding

--- src/main/java/ir/ac/aut/god/automatanewentries/core/ExcelReader.java
+++ src/main/java/ir/ac/aut/god/automatanewentries/core/ExcelReader.java
@@ -1,6 +1,7 @@
 package ir.ac.aut.god.automatanewentries.core;
 
 import com.google.gson.Gson;
+import ir.ac.aut.god.automatanewentries.model.CapSchool;
 import ir.ac.aut.god.automatanewentries.model.NeededClass;
 import ir.ac.aut.god.automatanewentries.model.PazireshType;
 import ir.ac.aut.god.automatanewentries.model.School;
@@ -8,15 +9,31 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
 public class ExcelReader {
 
+
+    public static void gout(Object object) {
+        System.out.println(new Gson().toJson(object));
+
+    }
+
     public static void main(String[] args) throws IOException, InvalidFormatException {
 
-        s();
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("t4_945_1045");
+        strings.add("t4_800_1000");
+        strings.add("t4_1000_1200");
+
+        ArrayList<String> strings1 = extractConfilicts(strings);
+
+        gout(strings1);
+
+
+        //    s();
+//        cap();
         System.exit(0);
 
 
@@ -104,8 +121,7 @@ public class ExcelReader {
                     }
                     neededClass.setPossibleGroups(groups);
 
-                }
-                else
+                } else
                     neededClass.setPossibleGroups(null);
 
                 courseId = dataFormatter.formatCellValue(row.getCell(2));
@@ -136,6 +152,80 @@ public class ExcelReader {
 
         workbook.close();
 
+    }
+
+
+    public static ArrayList<CapSchool> cap() throws IOException, InvalidFormatException {
+
+        ArrayList<CapSchool> capSchools = new ArrayList<>();
+
+        String SAMPLE_XLSX_FILE_PATH = "conf/cap.xlsx";
+        Workbook workbook = WorkbookFactory.create(new File(SAMPLE_XLSX_FILE_PATH));
+        Sheet sheet = workbook.getSheetAt(0);
+        DataFormatter dataFormatter = new DataFormatter();
+        for (Row row : sheet) {
+            String schoolName = dataFormatter.formatCellValue(row.getCell(0))
+                    .replaceAll("ي", "ی");
+            String cosCode = dataFormatter.formatCellValue(row.getCell(1));
+            String paziresh = dataFormatter.formatCellValue(row.getCell(3));
+            String cap = dataFormatter.formatCellValue(row.getCell(4));
+
+            if (schoolName.isEmpty())
+                continue;
+
+
+            CapSchool capSchool = new CapSchool().setSchoolName(schoolName)
+                    .setChertCode(cosCode).setCap(Integer.parseInt(cap));
+
+            if (paziresh.equals("روزانه"))
+                capSchool.setPazireshType(PazireshType.Awdi);
+            else if (paziresh.equals("پرديس خودگردان"))
+                capSchool.setPazireshType(PazireshType.Pardis);
+            else throw new RuntimeException("what is type??????????????");
+
+
+            capSchools.add(capSchool);
+
+            System.err.println(new Gson().toJson(capSchool));
+        }
+        return capSchools;
+
+    }
+
+
+    static public ArrayList<String> extractConfilicts(ArrayList<String> times) {
+        ArrayList<String> response = new ArrayList<>();
+        for (String time : times) {
+            String[] s = time.split("_");
+            String numberofweek = s[0].substring(1);
+            String st = s[1];
+            String ft = s[2];
+            int startTime = (Integer.parseInt(numberofweek)) * 2000 + Integer.parseInt(st);
+            int finishTime = (Integer.parseInt(numberofweek)) * 2000 + Integer.parseInt(ft);
+
+            String tis = time + ":";
+
+            for (String tt : times) {
+                String[] tts = tt.split("_");
+                String ttnumberofweek = tts[0].substring(1);
+                String ttst = tts[1];
+                String ttft = tts[2];
+                int ttstartTime = (Integer.parseInt(ttnumberofweek)) * 2000 + Integer.parseInt(ttst);
+                int ttfinishTime = (Integer.parseInt(ttnumberofweek)) * 2000 + Integer.parseInt(ttft);
+
+                boolean b = !(
+                        ((ttstartTime < startTime) && (ttfinishTime <= startTime)) ||
+                                ((ttstartTime >= finishTime) && (ttfinishTime > finishTime))
+                );
+
+                boolean b0 = (startTime == ttstartTime) && (ttfinishTime == finishTime);
+                if (b && !b0) {
+                    tis += tt + ",";
+                }
+            }
+            response.add(tis);
+        }
+        return response;
     }
 
 }
