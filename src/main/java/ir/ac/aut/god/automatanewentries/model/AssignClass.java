ad63c665f313e820653e5f76c2bbb58ece47aaf3
95129032 <ag1992@aut.ac.ir>
2019-07-18 16:32:18 +0430

    commit

---
+++ src/main/java/ir/ac/aut/god/automatanewentries/model/AssignClass.java
@@ -0,0 +1,34 @@
+package ir.ac.aut.god.automatanewentries.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.util.ArrayList;
+
+/**
+ * created By aMIN on 5/12/2019 9:40 PM
+ */
+
+
+@Getter
+@Setter
+@Accessors(chain = true)
+public class Class {
+    private String name;
+    private String courseId;
+    private String id;
+    private String group;
+    private EntranceType entranceType;
+    private PazireshType pazireshType;
+    private SEX sex;
+    private ArrayList<TimeOf> InvolovingTimes;
+    private ArrayList<String> times;
+    private TimeOf finalExamTime;
+    private ExamTime examTime;
+    private int capacity;
+    private int blankCapacity;
+    private int maxCapacity;
+    private int minCapacity;
+
+}
